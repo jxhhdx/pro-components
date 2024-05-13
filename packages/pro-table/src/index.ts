f23a7919a54b8cdabc83affbd7de384c717d83fd
gxuud <igaoxian@foxmail.com>
2024-05-13 18:27:35 +0800

    docs: 基本的pro-table文档

--- packages/pro-table/src/index.ts
+++ packages/pro-table/src/index.ts
@@ -4,9 +4,9 @@ import type { App } from 'vue';
 
 export type { ProColumnType, ProColumnGroupType, ProColumnsType, ProTableProps, ResponseData } from './typings';
 
-ProTable.install = (app: App) => app.component(ProTable.name, ProTable);
+ProTable.install = (app: App) => app.component(ProTable.name as string, ProTable);
 
-EditableProTable.install = (app: App) => app.component(EditableProTable.name, EditableProTable);
+EditableProTable.install = (app: App) => app.component(EditableProTable.name as string, EditableProTable);
 
 export { EditableProTable, ProTable };
 
--- site/src/docs/pro-table/index.zh-CN.md
+++ site/src/docs/pro-table/index.zh-CN.md
@@ -6,3 +6,16 @@ cols: 2
 title: ProTable
 cover: https://gw.alipayobjects.com/zos/alicdn/qtJm4yt45/AutoComplete.svg
 ---
+
+ProTable 的诞生是为了解决项目中需要写很多 table 的样板代码的问题，所以在其中做了封装了很多常用的逻辑。这些封装可以简单的分类为预设行为与预设逻辑。
+
+## 何时使用
+
+当你的表格需要与服务端进行交互或者需要多种单元格样式时，ProTable 是不二选择。
+
+## API
+
+### ProTable
+
+| 属性 | 描述 | 类型 | 默认值 |
+| --- | --- | --- | --- |
